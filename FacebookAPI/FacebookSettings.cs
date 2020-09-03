c9e00f96ee468de855e04f960e485f76dbbede20
CristiStratulat <43218916+CristiStratulat@users.noreply.github.com>
2020-09-03 23:47:38 +0300

    Add project files.

---
+++ FacebookAPI/FacebookSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacebookChatBot.FacebookAPI
+{
+    public static class FacebookSettings
+    {
+        public static string AccessToken = "EABGZB8SL4i6cBAA6TmuFtkDZCcVJk063ldmZBZA3opZAqczzoZBE8Ee2ugP5PdrK4Fol2WcOYbD9QDBbNgzaJk2pGDCPYB8xrFVLxMD6Orc2TdZBgITZANbLrpNsHV6IalhmkpgxxxZCAtZCe1ruDZCJIuLaxeDPU0WOiEhy3BKOtxU5HeZCqlpvxN6Pu4efnSQU6TUZD";
+    }
+}
